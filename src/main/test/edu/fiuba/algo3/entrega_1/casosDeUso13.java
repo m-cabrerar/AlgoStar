2f04adc5f605efc3892eed6c41e1ccd03086228e
ppazb <ppaz@fi.uba.ar>
2022-11-09 23:24:54 -0300

    Caso 13
    Agregue import necesario

--- src/main/test/edu/fiuba/algo3/entrega_1/casosDeUso13.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/casosDeUso13.java
@@ -1,5 +1,5 @@
 package edu.fiuba.algo3.entrega_1;
-
+import org.junit.jupiter.api.Test;
 public class casosDeUso13 {
     /*Se destruye un criadero e igual se puede seguir construyendo sobre el moho que dejó.*/
     @Test
