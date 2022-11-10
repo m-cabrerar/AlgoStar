3ef51293af2b0dce4b3295116b6c051aae850e73
Mateo Cabrera <mcabrerar@fi.uba.ar>
2022-11-09 22:57:06 -0300

    import assertTrue

--- src/main/test/edu/fiuba/algo3/entrega_1/casosDeUso10.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/casosDeUso10.java
@@ -2,6 +2,8 @@ package edu.fiuba.algo3.entrega_1;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class casosDeUso10 {
     /*Verificar que al dañar una construcción zerg, la misma recupera la vida por turnos hasta
      *volver a tener el 100%.
