1dc59460f4804c25ba2b733c4a293e7cc2675a41
ppazb <ppaz@fi.uba.ar>
2022-11-09 23:12:47 -0300

    Update casosDeUso16.java

--- src/main/test/edu/fiuba/algo3/entrega_1/casosDeUso16.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/casosDeUso16.java
@@ -1,4 +1,7 @@
 package edu.fiuba.algo3.entrega_1;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class casosDeUso16 {
     /*- Verificar que no se pueda construir sobre un volcán con una edificación ya existente
@@ -7,5 +10,67 @@ public class casosDeUso16 {
      * viceversa (zángano no puede extraer mineral si ya tiene un nexo construido sobre el
      * nodo).
      */
+    @Test
+    public void Test01NoPuedoConstruirUnAsimiladorSobreOtro() {
+        //ARRANGE
+        String mensaje = "No se puede construir sobre otra construccion";
+        Casillero casillero = new NodoDeGas();
+        Banco banco = new Banco(300,300);
+        Edificio asimilador = new Asimilador(casillero, banco);
 
+        //ACT
+        for(int i=0; i<6; i++){
+            asimilador.pasarTurno();
+        }
+        Exception exception = assertThrows(Exception.class, () -> {
+            Edificio asimilador = new Asimilador(casillero, banco);
+        });
+        //ASSERT
+        assertEquals(mensaje, exception.getMessage());
+    }
+
+    @Test
+    public void Test02NoPuedoConstruirUnNexoMineralSobreUnNodoConZangano() {
+        //ARRANGE
+        String mensaje = "No se puede construir en un mineral donde estan trabajando";
+        Casillero casillero = new NodoMineral();
+        Banco banco = new Banco(300,300);
+        Criadero criadero = Criadero.inicializar();
+
+
+        //Supuesto
+        Zangano zangano = criadero.engendrarZangano();
+        zangano.recolectarMineral(casillero);
+        //TODO ZANGANO EXTRAER MINERAL EN ESA CASILLA
+
+        //ACT
+        Exception exception = assertThrows(Exception.class, () -> {
+            Edificio nexoMineral = new NexoMineral(casillero, banco);
+        });
+        //ASSERT
+        assertEquals(mensaje, exception.getMessage());
+    }
+
+    @Test
+    public void Test03NoPuedoMandarUnZanganoARecolectarDondeHayUnNexoMineral() {
+        //ARRANGE
+        String mensaje = "No se puede extraer donde hay construcciones";
+        Casillero casillero = new NodoMineral();
+        Banco banco = new Banco(300,300);
+        Edificio nexoMineral = new NexoMineral(casillero, banco);
+        for(int i=0; i<6; i++){
+            nexoMineral.pasarTurno();
+        }
+
+        Criadero criadero = Criadero.inicializar();
+        Zangano zangano = criadero.engendrarZangano();
+
+        //ACT
+        Exception exception = assertThrows(Exception.class, () -> {
+            zangano.recolectarMineral(casillero);
+            //TODO ZANGANO EXTRAER MINERAL EN ESA CASILLA
+        });
+        //ASSERT
+        assertEquals(mensaje, exception.getMessage());
+    }
 }
