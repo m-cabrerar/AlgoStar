2be62c3f01449611ee0bb827f7043821dcab775c
Cami <82735635+c-gonzalez-a@users.noreply.github.com>
2022-11-16 00:20:56 -0300

    Excepciones nuevas y la carpeta

---
+++ src/main/java/edu/fiuba/algo3/exceptions/UbicacionInvalida.java
@@ -0,0 +1,7 @@
+package edu.fiuba.algo3.exceptions;
+
+public class UbicacionInvalida extends Exception {
+    public UbicacionInvalida(String ubicacion_invalida) {
+        super(ubicacion_invalida);
+    }
+}
