b8eff47a389b48f60d22a00ab26c7255ae70523f
ppazb <ppaz@fi.uba.ar>
2022-11-16 03:33:27 -0300

    Actualizacion Mapa Casillero

--- src/main/java/edu/fiuba/algo3/modelo/TipoCasillero.java
+++
@@ -1,9 +0,0 @@
-package edu.fiuba.algo3.modelo;
-
-public interface TipoCasillero {
-
-    public void pasarTurno();
-    public String nombreDelCasillero();
-
-    void extraerMineral(Inventario inventario, int cantidad) throws Exception;
-}
