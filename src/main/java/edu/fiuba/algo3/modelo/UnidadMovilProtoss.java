1fa819829cdd9c2cf35f66ec6c05ea4b8c7e27d7
Mateo Cabrera <mcabrerar@fi.uba.ar>
2022-11-17 15:18:33 -0300

    Tipos de Unidades

---
+++ src/main/java/edu/fiuba/algo3/modelo/UnidadMovilProtoss.java
@@ -0,0 +1,27 @@
+package edu.fiuba.algo3.modelo;
+
+import edu.fiuba.algo3.exceptions.EstaDestruido;
+
+public abstract class UnidadMovilProtoss extends UnidadMovil {
+    int escudoMaximo;
+    int escudo;
+    int vida;
+
+    UnidadMovilProtoss(Inventario inventario, int costoMineral, int costoGas, int vidaInicial, int escudoInicial){
+        super(inventario, costoMineral, costoGas);
+        this.vida = vidaInicial;
+        this.escudo = escudoInicial;
+        this.escudoMaximo = escudoInicial;
+    }
+
+    @Override
+    public void pasarTurno() {
+        if (escudo < (escudoMaximo-10)){
+            escudo+=10;
+        }
+        else if( (escudo < escudoMaximo) && (escudo > (escudoMaximo-10)) ){
+            escudo = escudoMaximo;
+        }
+    }
+
+}
