c1469c83003c0af32346743eae7ae36ac18c14f9
Cami <82735635+c-gonzalez-a@users.noreply.github.com>
2022-11-17 18:30:38 -0300

    paso las pruebas a entrega2

--- src/main/test/edu/fiuba/algo3/entrega_2/ScoutTests.java
+++ src/main/test/edu/fiuba/algo3/entrega_2/ScoutTests.java
@@ -1,4 +1,4 @@
-package edu.fiuba.algo3.entrega_1;
+package edu.fiuba.algo3.entrega_2;
 
 import edu.fiuba.algo3.modelo.*;
 import org.junit.jupiter.api.Test;
@@ -6,38 +6,38 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.*;
 
-public class HidraliscoTests {
+public class ScoutTests {
     @Test
-    public void test01HidraliscoAtacaAUnidadVoladorYLeHace10DeDanio() {
+    public void test01ScoutAtacaAUnidadVoladorYLeHace14DeDanio() {
         //ARRANGE
         Inventario inventarioMock = mock(Inventario.class);
         when(inventarioMock.tieneRecursos(anyInt(),anyInt())).thenReturn(true);
         UnidadMovil unidadMock = mock(UnidadMovil.class);
         when(unidadMock.esVoladora()).thenReturn(true);
-        Hidralisco hidra = new Hidralisco(inventarioMock);
+        Scout scout = new Scout(inventarioMock);
         //ACT
         try {
-            hidra.atacar(unidadMock);
+            scout.atacar(unidadMock);
             //ASSERT
-            verify(unidadMock, times(1)).recibirDanio(10);
+            verify(unidadMock, times(1)).recibirDanio(14);
         } catch (Exception e) {
             fail();
         }
     }
 
     @Test
-    public void test02HidraliscoAtacaAUnidadTerrestreYLeHace10DeDanio() {
+    public void test02ScoutAtacaAUnidadTerrestreYLeHace8DeDanio() {
         //ARRANGE
         Inventario inventarioMock = mock(Inventario.class);
         when(inventarioMock.tieneRecursos(anyInt(),anyInt())).thenReturn(true);
-        Hidralisco hidralisco = new Hidralisco(inventarioMock);
         UnidadMovil unidadMock = mock(UnidadMovil.class);
         when(unidadMock.esVoladora()).thenReturn(false);
+        Scout scout = new Scout(inventarioMock);
         //ACT
         try {
-            hidralisco.atacar(unidadMock);
+            scout.atacar(unidadMock);
             //ASSERT
-            verify(unidadMock, times(1)).recibirDanio(10);
+            verify(unidadMock, times(1)).recibirDanio(8);
         } catch (Exception e) {
             fail();
         }
