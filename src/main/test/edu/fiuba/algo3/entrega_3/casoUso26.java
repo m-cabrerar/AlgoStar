7c767daf979ccaa04314473045d9c61f5db8606d
anitazarate <azarate@fi.uba.ar>
2022-11-19 13:32:05 -0300

    Update casoUso26.java

--- src/main/test/edu/fiuba/algo3/entrega_3/casoUso26.java
+++ src/main/test/edu/fiuba/algo3/entrega_3/casoUso26.java
@@ -3,6 +3,8 @@ package edu.fiuba.algo3.entrega_3;
 import edu.fiuba.algo3.modelo.*;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -13,12 +15,16 @@ correspondiente*/
 
     @Test
     public void Test01ProtossNopuedeConstruirDragonNoTieneSuministro(){
+        String mensaje = "No tiene suministros";
         Inventario mockedInventario = mock(Inventario.class);
         when(mockedInventario.tieneRecursos(anyInt(), anyInt())).thenReturn(true);
-
-        Dragon unDragon = new Dragon(mockedInventario);
-
-
-
+        when(mockedInventario.tieneSuministros(anyInt())).thenReturn(false);
+        //quien engengra las unidades?
+        //ACT
+        Exception exception = assertThrows(Exception.class, () -> {
+            Dragon unDragon = new Dragon(mockedInventario);
+        });
+        //ASSERT
+        assertEquals(mensaje, exception.getMessage());
     }
 }
