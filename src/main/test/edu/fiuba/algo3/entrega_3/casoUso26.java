d1fd7df6a9523140f210616f54f191a035b12505
Mateo Cabrera <mcabrerar@fi.uba.ar>
2022-11-25 11:00:28 -0300

    Update casoUso26.java

--- src/main/test/edu/fiuba/algo3/entrega_3/casoUso26.java
+++ src/main/test/edu/fiuba/algo3/entrega_3/casoUso26.java
@@ -11,12 +11,72 @@ public class casoUso26 {
 correspondiente*/
 
     @Test
-    public void Test01ProtossNopuedeConstruirDragonNoTieneSuministro(){
+    public void Test01NoSePuedenConstruirDragonSinSuministrosSuficientes(){
         //ARRANGE
         Inventario inventarioMock = mock(Inventario.class);
         when(inventarioMock.tieneRecursos(anyInt(), anyInt())).thenReturn(true);
         when(inventarioMock.tieneSuministros(anyInt())).thenReturn(false);
         //ACT & ASSERT
         assertThrows(SuministrosInsuficientes.class, () -> new Dragon(inventarioMock));
+    }
+
+    @Test
+    public void Test02NoSePuedenConstruirGuardianSinSuministrosSuficientes(){
+        //ARRANGE
+        Inventario inventarioMock = mock(Inventario.class);
+        when(inventarioMock.tieneRecursos(anyInt(), anyInt())).thenReturn(true);
+        when(inventarioMock.tieneSuministros(anyInt())).thenReturn(false);
+        //ACT & ASSERT
+        assertThrows(SuministrosInsuficientes.class, () -> new Guardian(inventarioMock));
+    }
+
+    @Test
+    public void Test03NoSePuedenConstruirHidraliscoSinSuministrosSuficientes(){
+        //ARRANGE
+        Inventario inventarioMock = mock(Inventario.class);
+        when(inventarioMock.tieneRecursos(anyInt(), anyInt())).thenReturn(true);
+        when(inventarioMock.tieneSuministros(anyInt())).thenReturn(false);
+        //ACT & ASSERT
+        assertThrows(SuministrosInsuficientes.class, () -> new Hidralisco(inventarioMock));
+    }
+
+    @Test
+    public void Test04NoSePuedenConstruirMutaliscoSinSuministrosSuficientes(){
+        //ARRANGE
+        Inventario inventarioMock = mock(Inventario.class);
+        when(inventarioMock.tieneRecursos(anyInt(), anyInt())).thenReturn(true);
+        when(inventarioMock.tieneSuministros(anyInt())).thenReturn(false);
+        //ACT & ASSERT
+        assertThrows(SuministrosInsuficientes.class, () -> new Mutalisco(inventarioMock));
+    }
+
+    @Test
+    public void Test05NoSePuedenConstruirScoutSinSuministrosSuficientes(){
+        //ARRANGE
+        Inventario inventarioMock = mock(Inventario.class);
+        when(inventarioMock.tieneRecursos(anyInt(), anyInt())).thenReturn(true);
+        when(inventarioMock.tieneSuministros(anyInt())).thenReturn(false);
+        //ACT & ASSERT
+        assertThrows(SuministrosInsuficientes.class, () -> new Scout(inventarioMock));
+    }
+
+    @Test
+    public void Test06NoSePuedenConstruirZealotSinSuministrosSuficientes(){
+        //ARRANGE
+        Inventario inventarioMock = mock(Inventario.class);
+        when(inventarioMock.tieneRecursos(anyInt(), anyInt())).thenReturn(true);
+        when(inventarioMock.tieneSuministros(anyInt())).thenReturn(false);
+        //ACT & ASSERT
+        assertThrows(SuministrosInsuficientes.class, () -> new Zealot(inventarioMock));
+    }
+
+    @Test
+    public void Test07NoSePuedenConstruirZerlingSinSuministrosSuficientes(){
+        //ARRANGE
+        Inventario inventarioMock = mock(Inventario.class);
+        when(inventarioMock.tieneRecursos(anyInt(), anyInt())).thenReturn(true);
+        when(inventarioMock.tieneSuministros(anyInt())).thenReturn(false);
+        //ACT & ASSERT
+        assertThrows(SuministrosInsuficientes.class, () -> new Zerling(inventarioMock));
     }
 }
