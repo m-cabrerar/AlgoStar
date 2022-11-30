ed2d393f7adfc0d36616224bfd6087a5a0272d48
ppazb <ppaz@fi.uba.ar>
2022-11-30 01:10:45 -0300

    Refactor Casillero

--- src/main/java/edu/fiuba/algo3/modelo/CasilleroEspacial.java
+++ src/main/java/edu/fiuba/algo3/modelo/CasilleroEspacial.java
@@ -1,13 +1,11 @@
 package edu.fiuba.algo3.modelo;
-
-import edu.fiuba.algo3.exceptions.CasilleroNoCompatible;
 import edu.fiuba.algo3.exceptions.UbicacionInvalida;
 
 public class CasilleroEspacial extends TipoCasillero {
     @Override
     public boolean ocupar(UnidadMovil unidad) throws UbicacionInvalida {
         if (unidad.esVoladora()){return true;}
-        throw new CasilleroNoCompatible("Ubicacion invalida");
+        throw new UbicacionInvalida("Ubicacion invalida");
     }
 
 }
