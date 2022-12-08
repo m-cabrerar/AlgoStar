a5b7adfc3f80a5295271e71a0bfbf3b3c1912343
ppazb <ppaz@fi.uba.ar>
2022-12-08 08:38:21 -0300

    casillero expansion

--- src/main/java/edu/fiuba/algo3/modelo/casillero/Moho.java
+++ src/main/java/edu/fiuba/algo3/modelo/casillero/Moho.java
@@ -12,6 +12,9 @@ public class Moho extends TipoCasillero {
             if (casilla.estaOcupado()) {
                 continue;
             }
+            int posx = casilla.posicionX();
+            int posy = casilla.posicionY();
+            System.out.println("Se contagia: " + posx + posy);
             casilla.volverseMoho();
         }
     }
